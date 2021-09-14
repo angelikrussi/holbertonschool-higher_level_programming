b145a934d769cfe8fcf20710b268942eccb7c651
angelikrussi <hannamrrussi@gmail.com>
2021-09-13 22:29:53 -0500

    10. Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_n = []
+    for n in my_list:
+       if (n % 2) == 0:
+        list_n.append(True)
+       else:
+         list_n.append(False)
+    return list_n

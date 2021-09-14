7ba621f4ca109277a6b788882a31ec7a67baec96
angelikrussi <hannamrrussi@gmail.com>
2021-09-13 20:03:32 -0500

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
 
-    if first == 0:
+    if length == 0:
         return(length, None)
+    first = sentence[0]
     return(length, first)

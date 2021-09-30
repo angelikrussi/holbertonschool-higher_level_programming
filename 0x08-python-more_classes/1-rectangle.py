fed31400f7bdbd8cbbe491de3433ea7059d113b5
angelikrussi <hannamrrussi@gmail.com>
2021-09-29 20:29:03 -0500

    1. Definición real de un rectángulo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,4 +31,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__size = value
+        self.__height = value

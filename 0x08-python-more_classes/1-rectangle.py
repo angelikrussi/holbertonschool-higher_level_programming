22a5f2a757b13bbf33744b99715a2ccd1de2809a
angelikrussi <hannamrrussi@gmail.com>
2021-09-29 20:32:51 -0500

    1. Definición real de un rectángulo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle"""
+'''Write a class Rectangle that defines a
+rectangle by: (based on 0-rectangle.py)'''
 
 
 class Rectangle:
-    """An empty Square Rectangle"""
+    '''class Rectangle'''
     def __init__(self, width=0, height=0):
-        "initilizes"
+        '''Initializes the instance'''
         self.width = width
         self.height = height
 

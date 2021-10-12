2f6728ed32c158bd946923090d251a5bc5e1d790
angelikrussi <hannamrrussi@gmail.com>
2021-10-11 22:31:08 -0500

    9. Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,16 +12,32 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates that value is an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
+            raise ValueError("{} must be greater than 0".format(name))
+            
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
     def __init__(self, width, height):
         """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
+
+    def area(self):
+        """Area function
+        Returns:
+            The area of the rectangle
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """The function for use in print() and str()
+        Returns:
+            Specially formated string
+        """
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))

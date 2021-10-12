37c7506d80c7664b1f4ffa4429619162dfc27c16
angelikrussi <hannamrrussi@gmail.com>
2021-10-11 20:05:53 -0500

    3. Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,8 +3,10 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """"Verify if the object  is exactly an instance of the specified class"""
-    if type(obj,a_class):
+    """"Verify if the object  is exactly
+     an instance of the specified class"""
+
+    if isinstance(obj, a_class):
         return True
     else:
         return False

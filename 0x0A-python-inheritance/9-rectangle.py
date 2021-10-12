5bdbc3563d35ebd0e291cbb8671b45cfbb0da9d4
angelikrussi <hannamrrussi@gmail.com>
2021-10-11 22:35:43 -0500

    8. Rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class BaseGeometry
+Contains the class BaseGeometry and subclass Rectangle
 """
 
 
@@ -12,32 +12,25 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates that value is an integer greater than 0"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-            
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
     def __init__(self, width, height):
         """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Area function
-        Returns:
-            The area of the rectangle
-        """
-
+        """returns the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """The function for use in print() and str()
-        Returns:
-            Specially formated string
-        """
-
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)

90b66a3adb95c767e9ba0431ea14d8952c51f6be
Your Name <hannamrrussi@gmail.com>
2021-12-13 20:17:32 -0500

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const nA = process.argv;
+if (nA.length === 2) {
+    console.log('No argument');
+} else if (nA.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}

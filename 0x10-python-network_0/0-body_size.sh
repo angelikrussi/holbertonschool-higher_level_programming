8556b1e32219dbb48a1a23e99f2875b302a65cfd
angelikrussi <hannamrrussi@gmail.com>
2022-01-08 22:14:53 -0500

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #script that takes in a URL, sends a request to that URL, 
 #and displays the size 
-curl -sI $1 | grep "Content-Length" | cut -d ' ' -f 2
+curl "$1" -sI |grep "Content-Length"| cut -d ':' -f 2 | cut -d ' ' -f 2

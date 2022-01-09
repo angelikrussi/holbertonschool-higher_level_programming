3d14633ceced40b1924ba5d9bbb4f162a99154f2
angelikrussi <hannamrrussi@gmail.com>
2022-01-08 22:40:11 -0500

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
- Find a peak
-"""
+""" Find a peak."""
 
 
 def find_peak(list_of_integers):
-    """function Find a peak"""
-    list = list_of_integers
-    if list:
-        list.sort()
-        return list[-1]
+    """funtion finds  peak """
+    lis = list_of_integers
+    if lis:
+        lis.sort()
+        return lis[-1]
     else:
         return None

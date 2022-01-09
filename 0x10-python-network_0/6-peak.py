26e44ed1691673c3cb8f0dab532217009b008db3
angelikrussi <hannamrrussi@gmail.com>
2022-01-08 22:07:29 -0500

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+ Find a peak
+"""
+
+def find_peak(list_of_integers):
+    list= list_of_integers
+    if list:
+        list.sort()
+        return list[-1]
+    else:
+        return None

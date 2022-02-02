7b3ed9e41152b1d0ce3353163ae2c8aa77f2e781
angelikrussi <hannamrrussi@gmail.com>
2022-02-01 21:40:16 -0500

    Tasks 1 v-4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const File = process.argv[2];
-fs.readFile(File, 'utf8', function(err, data) {
+fs.readFile(File, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {

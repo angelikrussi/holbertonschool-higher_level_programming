095f0c06a967d3ce0b9bd5ae357a47cc5a61a93d
angelikrussi <85516749+angelikrussi@users.noreply.github.com>
2022-02-01 21:38:09 -0500

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv;
-
-fs.readFile(file, 'utf8', function (err, line) {
+const File = process.argv[2];
+fs.readFile(File, 'utf8', function(err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-  console.log(line);
+    console.log(data);
   }
 });

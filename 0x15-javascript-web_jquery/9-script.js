00e80f2bf2f8228a4732bc67f952155573a1a979
angelikrussi <hannamrrussi@gmail.com>
2022-02-14 22:09:21 -0500

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+//Say Hello!
+$.ajax({
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    type: 'GET',
+    dataType: 'json'
+  })
+    .done(function (json) {
+      $('DIV#hello').text(json.hello);
+    });
+ 
